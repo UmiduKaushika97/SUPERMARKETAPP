8af28dd3eaae4cabff9051ec6af28c989c86dd87
UmiduKaushika97 <umidu.kaushika@gmail.com>
2026-09-14 22:58:46 +0400

    nav link create

--- src/components/MainNavBar.tsx
+++ src/components/MainNavBar.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navimage from '../Images/imagesgrand.png'
 
+
+
 const MainNavBar = () => {
+
+const [menuOpen, setMenuOpen] = useState(false);
+
+     const onToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <header className='bg-white'>
@@ -10,7 +19,7 @@ const MainNavBar = () => {
             <img src={Navimage} className='w-16'/>
           </div>
 
-          <div className='md:static absolute min-h-[60vh] left-0 top-[9%] w-full flex items-center px-5 '>
+          <div className='nav-links md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[-100%] w-auto flex items-center px-5 '>
             <ul className='flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8'>
 
               <li>
@@ -32,12 +41,16 @@ const MainNavBar = () => {
 
           </div>
 
-          <div>
+          <div className='flex items-center gap-6'>
             <button className='bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]'>Sign In</button>
+            <i className='text-3xl cursor-pointer md:hidden
+            onclick={onToggleMenu(this)}'>
+              <ion-icon name={menuOpen ? "close-outline" : "menu-outline"} ></ion-icon>
+              </i>
           </div>
 
         </nav>
-        <h1>rtfyf</h1>
+       
       </header>
     </>
   )
